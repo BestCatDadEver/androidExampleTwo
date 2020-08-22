d059dcc84a5a7c88e2584bbb335b08ab6530e4bb
Carlos <cgelpi1439@gmail.com>
2020-08-21 23:00:45 -0300

    Using RecyclerView.

--- app/src/main/java/com/example/exampletwo/Controller/MainActivity.kt
+++ app/src/main/java/com/example/exampletwo/Controller/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.Dataset
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exampletwo.Adapters.CategoryAdapter
+import com.example.exampletwo.Adapters.CategoryRecycleAdapter
 import com.example.exampletwo.Model.Category
 import com.example.exampletwo.R
 import com.example.exampletwo.Services.DataService
@@ -12,14 +14,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this,DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
     }
 }
